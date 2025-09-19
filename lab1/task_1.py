5719883c18f50c813b468764b423ab98b842e06a
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-09-19 15:30:26 +0300

    Add task_3 lab1

--- lab1/task_1.py
+++ lab1/task_1.py
@@ -1,5 +1,5 @@
 print("Введите фамилию, имя и отчество")
-#Объясляем переменные с помощью множественно присваивания
+
 surname, name, father_name = input(), input(), input()
-#Форматируем и выводим строку в вид ФИО
+
 print(surname.title(), name[0].upper() + ".", father_name[0].upper()  + ".")
