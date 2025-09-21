eab7f07b7b014851d0a197b6eece9df1909a7ea4
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-09-21 22:40:11 +0300

    Delete all

--- lab1/task_1.py
+++
@@ -1,6 +0,0 @@
-print("Введите фамилию, имя и отчество")
-
-surname, name, father_name = input(), input(), input()
-
-print(surname.title(), name[0].upper() + ".", father_name[0].upper()  + ".")
-
--- lab1/task_8.py
+++
@@ -1,7 +0,0 @@
-print("Введите строку:")
-
-string = input()
-formatted_str = string.replace(" ", "").lower()
-
-if formatted_str == formatted_str[::-1]:
-    print("Палиндром:", string)
