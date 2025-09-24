5fff0ee42288d5b3758c5ff1a88706826517a16d
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-09-24 08:34:33 +0300

    Fix task_2 and task_9

--- lab_1/task_9.py
+++ lab_1/task_9.py
@@ -1,19 +1,30 @@
-print("Введите IP-адрес:")
-ip_address = input()
+ip_address = input("Введите IP-адрес: ")
 split_address = ip_address.split(".")
 
-if len(split_address) != 4:
-    print("Похоже, это совсем не IP-адрес:(")
-else:
-    is_valid = True
-    for num in split_address:
-        if not num.isdigit():
-            is_valid = False
-            break
-        elif 0 > int(num) < 255:
-            is_valid = False
-            break
-    if is_valid:
-        print("Ваш IP-адрес верный!")       
+if len(split_address) == 4:
+    if (
+        split_address[0].isdigit() and 0 <= int(split_address[0]) <= 255 and
+        split_address[1].isdigit() and 0 <= int(split_address[1]) <= 255 and
+        split_address[2].isdigit() and 0 <= int(split_address[2]) <= 255 and
+        split_address[3].isdigit() and 0 <= int(split_address[3]) <= 255
+    ):
+        print("Корректный IP-адрес")
     else:
-        print("В вашем адресе есть ошибка:(")        
+        print("В вашем адресе есть ошибка:(")
+else:
+    print("В вашем адресе есть ошибка:(")
+# if len(split_address) != 4:
+#     print("Похоже, это совсем не IP-адрес:(")
+# else:
+#     is_valid = True
+#     for num in split_address:
+#         if not num.isdigit():
+#             is_valid = False
+#             break
+#         elif 0 > int(num) < 255:
+#             is_valid = False
+#             break
+#     if is_valid:
+#         print("Ваш IP-адрес верный!")       
+#     else:
+#         print("В вашем адресе есть ошибка:(")        
