6e8cd80b2dead11d6e7f8fbea0682cf9137d6a5f
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-09-27 17:39:45 +0300

    Add task_7 lab_2_1

---
+++ lab_2/lab_2_1/task_7.py
@@ -0,0 +1,16 @@
+user_str = input()
+
+if user_str == "":
+    print(0)
+else:  
+    result_str = ""
+    counter = 1
+    for i in range(1, len(user_str)):
+        if user_str[i] == user_str[i-1]:
+            counter += 1
+        else:
+            result_str += user_str[i-1] + str(counter)
+            counter = 1
+    result_str += user_str[-1] + str(counter)
+        
+    print(result_str)
