270e540091cc4b60fa0ea705f0a0350a32d67971
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-09-27 20:08:21 +0300

    Add task_2 lab_2_2

---
+++ lab_2/lab_2_2/task_2.py
@@ -0,0 +1,12 @@
+dict_a = { 'a': 1, 'b': { 'c': 1, 'f': 4 } }
+dict_b = { 'd': 1, 'b': { 'c': 2, 'e': 3 } } 
+
+def merge_dicts(dict1: dict, dict2: dict):
+    for key, value in dict2.items():
+        if key in dict1 and isinstance(dict1[key], dict) and isinstance(value, dict):
+            merge_dicts(dict1[key], value)
+        else:
+            dict1[key] = value
+
+merge_dicts(dict_a, dict_b) 
+print(dict_a)
