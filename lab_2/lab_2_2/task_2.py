035c84a51b9f436197b6010d395d6cda39f08e16
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-09-28 20:47:38 +0300

    Fix task_2

--- lab_2/lab_2_2/task_2.py
+++ lab_2/lab_2_2/task_2.py
@@ -1,12 +1,17 @@
-dict_a = { 'a': 1, 'b': { 'c': 1, 'f': 4 }, 'g': (1, 2) }
+dict_a = { 'a': 1, 'b': { 'c': 1, 'f': 4 } }
 dict_b = { 'd': 1, 'b': { 'c': 2, 'e': 3 } } 
 
 def merge_dicts(dict1: dict, dict2: dict):
     for key, value in dict2.items():
-        if key in dict1 and isinstance(dict1[key], dict) and isinstance(value, dict):
-            merge_dicts(dict1[key], value)
+        if key in dict1:
+            if isinstance(dict1[key], dict) and isinstance(value, dict):
+                merge_dicts(dict1[key], value)
+            elif isinstance(dict1[key], (int, float)) and isinstance(value, (int, float)):
+                dict1[key] = max(dict1[key], value)
+            else:
+                dict1[key] = value
         else:
             dict1[key] = value
-
+            
 merge_dicts(dict_a, dict_b) 
 print(dict_a)
