4d1ddcfa6954a004513869c6634f7f34a8f78013
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-10-05 19:15:27 +0300

    Continue bank app

--- lab_3/bank.py
+++ lab_3/bank.py
@@ -1,49 +1,117 @@
+from time import time
+
 class Bank:
     def __init__(self, name):
         self.name = name 
+        self.customers = {}
     
-    def create_account(self, customer, sum, currency):
-        customer.account.append(Account(sum, currency))
+    def add_customer(self, customer):
+        self.customers[customer.id] = customer
     
-    def close_account(self, customer):
-        customer.account = "Нет действующего счёта"
+    def create_account(self, customer, currency):
+        if customer.id not in self.customers:
+            raise ValueError("Клиента нет в списке банка.")
+        if currency in customer.accounts:
+            print("У клиента уже есть счёт в этой валюте.")
+        else:
+            account = Account(customer, currency)
+            customer.accounts[currency] = account
+            return account
     
-    def transfer_account(self, account1, account2, amount):
-        if amount > account1.sum or amount > account2.sum:
-            print("На одном из счетов недостаточно средств")
-        else: 
-            account1.sum - amount
-            account2.sum + amount
+    def close_account(self, customer, currency):
+        if customer.id not in self.customers:
+            print("Клиента нет в списке банка.")
+        if currency not in customer.accounts:
+            print("Такого счёта у клиента нет.")
+        else:
+            del customer.accounts[currency]
+    
+    def add_to_account(self, customer, currency, amount):
+        account = self._get_account(customer, currency)
+        account.add(amount)
+            
+    def get_from_account(self, customer, currency, amount):
+        account = self._get_account(customer, currency)
+        account.get(amount)
+    
+    def transfer_to_account(self, from_customer, to_customer, currency, amount):
+        from_account = self._get_account(from_customer, currency)
+        to_account = self._get_account(to_customer, currency)
+        from_account.get(amount)
+        to_account.add(amount)
+            
+    def print_customers(self):
+        print("Клиенты банка:")
+        for customer in self.customers.values():
+            print(f"{customer.name} {customer.surname}, {customer.age}")
+       
+    def _get_account(self, customer, currency):
+        if customer.id not in self.customers:
+            print("Клиента нет в списке банка.")
+        if currency not in customer.accounts:
+            print("У клиента нет счёта в этой валюте.")
+        else:
+            return customer.accounts[currency]
     
 class Account:
-    def __init__(self, sum, currency):
+    def __init__(self, customer, currency):
+        self.customer = customer
         self.currency = currency
-        self.sum = sum
+        self.sum = 0
         
     def add(self, amount):
-        self.sum + amount
+        if amount <= 0:
+            raise ValueError("Введите положительное число")
+        self.sum += amount
     
     def get(self, amount):
+        if amount <= 0:
+            raise ValueError("Введите положительное число")
         if amount > self.sum:
             print("На счету недостаточно средств")
         else:
-            self.sum - amount
+            self.sum -= amount
             
 class Customer:
-    def __init__(self, id, name, surname, age):
-        self.id = id
+    def __init__(self, name, surname, age):
+        self.id = time()
         self.name = name
         self.surname = surname
         self.age = age
-        self.account = []
+        self.accounts = {}
+        
+    def print_customer(self):
+        print(f"{self.id}. {self.name} {self.surname}, {self.age}")
+        
+    def print_accounts(self):
+        if not self.accounts:
+            print("  Нет открытых счетов.")
+        else:
+            print(f"Счета клиента {self.name}:")
+            for currency in self.accounts.keys():
+                print(f"Валюта: {currency}, сумма: {self.accounts[currency].sum}")
+            
+belarusbank = Bank("Беларусбанк")
+customer1 = Customer("Alex", "John", 21)
+customer2 = Customer("Sarah", "Brown", 22)
 
-belarusbank = Bank("Беларусбанк")        
-new_customer = Customer("0", "Alex", "John", 21)
-print(f"{new_customer.id}. {new_customer.name} {new_customer.surname}")
-belarusbank.create_account(new_customer, 100, "рубль")
-print(belarusbank.name)
-print(new_customer.account[0].sum) 
-new_customer.account[0].add(20)
-print(new_customer.account[0].sum) 
-belarusbank.create_account(new_customer, 100, "евро")
-print(new_customer.account[1].currency) 
+belarusbank.add_customer(customer1)
+belarusbank.add_customer(customer2)
+belarusbank.print_customers()
+
+belarusbank.create_account(customer1, "BYN")
+customer1.print_accounts()
+
+belarusbank.create_account(customer2, "BYN")
+customer2.print_accounts()
+
+belarusbank.add_to_account(customer1, "BYN", 100)
+customer1.print_accounts()
+
+belarusbank.add_to_account(customer2, "BYN", 200)
+customer2.print_accounts()
+
+belarusbank.transfer_to_account(customer1, customer2, "BYN", 50)
+print("\nПосле перевода:")
+customer1.print_accounts()
+customer2.print_accounts()
