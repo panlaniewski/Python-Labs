97ee8b6b35a4dff764a67088696b33f3af3f8fa6
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-10-27 16:40:04 +0300

    Finished task_2

--- lab_4/lab_4_2/task_2.py
+++ lab_4/lab_4_2/task_2.py
@@ -1,16 +1,22 @@
 import numpy as np
 
 user_distances = input("Введите длины участков: ").split(" ")
-user_speed = input("Введите скорости на кажом из участков: ").split(" ")
+user_speed = input("Введите скорости на каждом из участков: ").split(" ")
 
-distances = np.array([int(x) for x in user_distances])
-speed = np.array([int(x) for x in user_speed])
+distances = np.array(user_distances).astype(np.float64)
+speed = np.array(user_speed).astype(np.float64)
 
-start_k = int(input("Введите номер начального участка: ")) 
-end_p = int(input("Введите номер конечного участка: ")) + 1
+k = int(input("Введите номер начального участка: ")) 
+p = int(input("Введите номер конечного участка: ")) 
 
-full_way = np.sum(distances[start_k:end_p])
-full_time = np.sum(distances[start_k:end_p] / speed[start_k:end_p]).round(2)
-average_speed = np.mean(speed[start_k:end_p])
-
-print(f"S = {full_way} км, T = {full_time} часа, V = {average_speed} км/ч")
+if distances.size == speed.size and k >= 1 and p >= k and p <= distances.size:
+    selected_distances = distances[k:p+1]
+    selected_speeds = speed[k:p+1]
+    
+    full_way = np.sum(selected_distances)
+    full_time = np.sum(selected_distances / selected_speeds)
+    average_speed = full_way / full_time
+    
+    print(f"S = {full_way} км, T = {full_time:.2f} час, V = {average_speed:.2f} км/ч")
+else:
+    print("Некоторые введённые данные не корректны!")
