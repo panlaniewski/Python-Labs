aed20822dd24ea8a6d9d9c12aff0620f19717e2d
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-11-03 20:44:43 +0300

    Some conflict fixes task_1

--- lab_2/lab_2_1/task_1.py
+++ lab_2/lab_2_1/task_1.py
@@ -1,11 +1,13 @@
 text = input("Введите текст: ")
 
-sep_chr = ",.:?!-()';"
+sep_chr = ",.:?!-()'"
 text_lst = ''.join([char for char in text if char not in sep_chr]).lower().split()
+words_dict = { word.strip(): text_lst.count(word) for word in text_lst if word != ""}
+unique_words = 0
 
-words_dict = {}
-for word in text_lst:
-    words_dict[word] = words_dict.get(word, 0) + 1
+for key in words_dict:
+    if words_dict[key] == 1:
+       unique_words += 1 
 
 print("Словарь слов текста:", words_dict)
-print("Количество уникальных слов:", len(words_dict)) 
+print("Количество уникальных слов:", unique_words)  
