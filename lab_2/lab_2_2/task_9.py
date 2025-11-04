a9c93e58d4dc26cdf6143b61fd580e98279bc075
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-11-04 14:06:40 +0300

    Fix task_9

--- lab_2/lab_2_2/task_9.py
+++ lab_2/lab_2_2/task_9.py
@@ -2,9 +2,9 @@ def type_check(*types):
     def dec(func):
         def wrapper(*args, **kwargs):
             try:
-                for arg, arg_type in zip(args, types):
+                for i, (arg, arg_type) in enumerate(zip(args, types)):
                     if not isinstance(arg, arg_type):
-                        raise TypeError(f"Ожидался тип {arg_type.__name__}, а получен {type(arg).__name__}")
+                        raise TypeError(f"Аргумент {i+1} ('{arg}') имеет неверный тип. Ожидался {arg_type.__name__}, а получен {type(arg).__name__}")
                 return func(*args, **kwargs)
             except TypeError as error:
                 return f"Ой, ошибка типов! {error}"
@@ -15,8 +15,7 @@ def type_check(*types):
 def add(a, b):
     return a + b
 
-print(add(2, 2))
-
-
-
-
+print(add(2, 2))        
+print(add(2, "2"))     
+print(add("2", 2))     
+print(add("2", "2"))    
