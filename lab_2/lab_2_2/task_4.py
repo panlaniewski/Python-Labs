b347f0d3a33bfa301dc2706a889d140295bb492b
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-11-04 14:02:16 +0300

    Fix task_4

--- lab_2/lab_2_2/task_4.py
+++ lab_2/lab_2_2/task_4.py
@@ -2,34 +2,29 @@ print("Введите размеры матрицы:")
 rows, cols = int(input("Количество строк: ")), int(input("Количество столбцов: "))
 
 matrix = []
+print(f"Введите матрицу {rows} на {cols} построчно:")
 for i in range(rows):
-    row = input().split(" ")
-    if len(row) == rows:
-        matrix.append([int(x) for x in row])
-        raise IndexError("Несоответствие размеров строки")
-    elif len(row) < rows:
-        print(f"Недостаточно элементов в строке! Должно быть {rows}")
-    else:
-        print(f"Слишком много элементов в строке! Должно быть {rows}")
-    
+    row = input().split()
+    matrix.append([int(x) for x in row[:cols]])
+
 print("Исходная матрица:")
 for i in range(rows):
     for j in range(cols):
         print(matrix[i][j], end=" ")
     print()
-    
-def transpose(matrix: list):
+
+def transpose(matrix):
     n, m = len(matrix), len(matrix[0])
-    transpose = [[0]*n for _ in range(n)]
+    transpose = [[0]*n for _ in range(m)]
     for i in range(n):
         for j in range(m):
             transpose[j][i] = matrix[i][j]
     return transpose
 
-matrix = transpose(matrix)
+transposed = transpose(matrix)
 
 print("Транспонированная матрица:")
-for i in range(rows):
-    for j in range(cols):
-        print(matrix[i][j], end=" ")
+for i in range(len(transposed)):
+    for j in range(len(transposed[0])):
+        print(transposed[i][j], end=" ")
     print()
