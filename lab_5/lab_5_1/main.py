b39f0b9bcaffa0f295c4020904a174241d5f3f61
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-11-18 12:04:20 +0300

    Add test request

--- lab_5/lab_5_1/main.py
+++ lab_5/lab_5_1/main.py
@@ -1,2 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
+
+url = 'https://en.wikipedia.org/wiki/Belarus'
+headers = {
+    "User-Agent": "'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Mobile Safari/537.36'"
+}
+response = requests.get(url, headers=headers)
+response.raise_for_status()
+print(f"Статус: {response.status_code}")
+print(response.text)
