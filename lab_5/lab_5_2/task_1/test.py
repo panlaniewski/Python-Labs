ee07ce40e05591df3ac7e6b4febfb8676217cc2c
Alyaksey Lanewski <lanevskialex1996@gmail.com>
2025-12-15 10:11:48 +0300

    Parametrize task_1

--- lab_5/lab_5_2/task_1/test.py
+++ lab_5/lab_5_2/task_1/test.py
@@ -1,28 +1,43 @@
 from main import count_words
+import pytest 
 
-def test_sentence():
-    assert count_words("Hello Python, hello pytest!") == 4
+@pytest.mark.parametrize("str, res", [
+    ("Hello Python, hello pytest!", 4),
+    ("Hello", 1),
+    ("", 0),
+    ("   ", 0),
+    ("  \t  \n  ", 0),
+    ("  \t  \n  ", 0),
+    ("Hello    it's    Python", 3),
+    ("  Hello  it's  Python  ", 3),
+    ("Hello, world! How are you?", 5),
+    ("Hello\tWorld\ntest", 3),
+    ("1 2 3 4 5", 5),
+    ("2024 year", 2),
+])
+def test_sentence(str, res):
+    assert count_words(str) == res
     
-def test_single_word():
-    assert count_words("Hello") == 1
+# def test_single_word():
+#     assert count_words("Hello") == 1
 
-def test_empty_string():
-    assert count_words("") == 0
+# def test_empty_string():
+#     assert count_words("") == 0
 
-def test_only_spaces():
-    assert count_words("   ") == 0
-    assert count_words("  \t  \n  ") == 0
+# def test_only_spaces():
+#     assert count_words("   ") == 0
+#     assert count_words("  \t  \n  ") == 0
 
-def test_multiple_spaces():
-    assert count_words("Hello    it's    Python") == 3
-    assert count_words("  Hello  it's  Python  ") == 3
+# def test_multiple_spaces():
+#     assert count_words("Hello    it's    Python") == 3
+#     assert count_words("  Hello  it's  Python  ") == 3
 
-def test_with_punctuation():
-    assert count_words("Hello, world! How are you?") == 5
+# def test_with_punctuation():
+#     assert count_words("Hello, world! How are you?") == 5
 
-def test_mixed_whitespace():
-    assert count_words("Hello\tWorld\ntest") == 3
+# def test_mixed_whitespace():
+#     assert count_words("Hello\tWorld\ntest") == 3
 
-def test_numbers_as_words():
-    assert count_words("1 2 3 4 5") == 5
-    assert count_words("2024 year") == 2
+# def test_numbers_as_words():
+#     assert count_words("1 2 3 4 5") == 5
+#     assert count_words("2024 year") == 2
